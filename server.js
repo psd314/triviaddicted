d301524a3b73f5219f0f11207874b44797cd53ff
Philippe Dixon <pdixon555@hotmail.com>
2018-02-15 20:54:48 -0500

    test deployment to heroku without css modules and scriptes ejected

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static("client/build"));
+
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
